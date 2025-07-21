7d5ca8760b9a91c46a2c09a2a6f940c04928fbaf
PURUSHATTOM <purushattom1999@gmail.com>
2025-07-21 23:04:59 +0530

    Commit 1

---
+++ src/main/java/org/example/Records/Runner.java
@@ -0,0 +1,12 @@
+package org.example.Records;
+
+public class Runner {
+    public static void main(String[] args) {
+        Student obj=new Student("Purushattom",26);
+
+        System.out.println(obj.name());
+        System.out.println(obj.age());
+        System.out.println(obj);
+
+    }
+}
---
+++ src/main/java/org/example/Records/Student.java
@@ -0,0 +1,3 @@
+package org.example.Records;
+
+public record Student(String name , int age ) {}
